aecc7716f707bb29f7ebb9a2e08214c9f912f7f6
rEq10000 <rarz11@yahoo.com>
2020-05-18 13:09:28 +0300

    Main File

---
+++ CamFetcher.py
@@ -0,0 +1,24 @@
+import datetime
+import urllib.request
+import time
+import os
+
+os.system("title NameHereCamFetcher")
+
+while True:
+
+    url = "urlhere"
+
+    times = datetime.datetime.now().strftime('%H.%M.%S')
+    date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    timerr = (date + " " + times)
+    full_name = ("NameHereIMG_" + timerr + ".jpg")
+
+    urllib.request.urlretrieve(url, full_name)
+
+    print("Image downloaded! Image: " + full_name)
+    print("Next picture in 15 minutes...")
+    print("")
+
+    time.sleep(900)
