5c57abfbb425653c8f4e9c639d4a65b0c5abfde7
rEq10000 <rarz11@yahoo.com>
2021-02-14 14:36:05 +0200

    Update CamFetcherHTML.py

--- CamFetcherHTML.py
+++ CamFetcherHTML.py
@@ -22,13 +22,15 @@ while True:
     urllib.request.urlretrieve(url, full_name)
 
     curvalue = 1 + curvalue
+	
+	sleeptime = 900
 
     with open ('CamFetcher.html', 'w') as f:
-        f.write('<html> <body> <h1>' + camname + 'CamFetcher</h1> <ul> <li>Last download: ' + timerr + '</li> <li>Images downloaded this session: ' + str(curvalue) + '</li> </ul> <h2>WCF Edition: 4, HTML Page Version: 2</h2> </body> </html>')
+        f.write('<html> <body> <h1>' + camname + 'CamFetcher</h1> <ul> <li>Last download: ' + timerr + '</li> <li>Images downloaded this session: ' + str(curvalue) + '</li> <li> Image URL: ' + url + '</li> <li> Next picture in ' + str(sleeptime) + ' seconds</li> </ul> <p>Last Image Downloaded:</p> <img src="' + full_name + '" </body> </html>')
 
     print("Image downloaded! Image: " + full_name)
     print("Next picture in 15 minutes...")
     print("Images downloaded this session: " + str(curvalue))
     print("")
 
-    time.sleep(900)
+    time.sleep(sleeptime)
